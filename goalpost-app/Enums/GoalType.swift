897e5d7c679783eedaa9eac4cc39b0fcd9acf3ab
Mark Phillips <mphill05@gmail.com>
2017-12-08 14:50:18 -0800

    Completed Goal Post App

---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Mark Phillips on 12/8/17.
+//  Copyright © 2017 Team Nerd. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
